4ba506a67bc55b975960ba20bdd56c81a814ad65
northernjamie <northernjamie@gmail.com>
2018-03-07 20:33:32 +0000

    added images with same dimensions

--- Working_Script.R
+++ Working_Script.R
@@ -46,10 +46,14 @@ lsoa_la_imd_vingtile <- lsoa_la_imd_dedup %>%
 # Remve the scilly isles, because there's only 1 LSOA there, and it messes everything up
 lsoa_la_imd_vingtile_noscilly <- lsoa_la_imd_vingtile[which(lsoa_la_imd_vingtile$la != "E06000053"), ]
 
+# Number of plots per row
+pperrow <- 16
+
+
 # Draw the plot
-g <- ggplot(lsoa_la_imd_vingtile_noscilly, aes(la_name, vingtile, fill=la_name))
-g <- g + facet_wrap(~la_name, strip.position = 'bottom', scales = 'free_x',ncol = 15)
-g <- g + geom_violin(color="white") + ggtitle("England Local Authority Deprivation Profiles",subtitle="These charts show the distribution of Lower Super Output Areas by deprivation in GM local authorities. \nA fatter bottom indicates proportionally more more-deprived LSOAs. A fatter head indicate more less-deprived LSOAs. \n\n@northernjamie ") +
+e <- ggplot(lsoa_la_imd_vingtile_noscilly, aes(la_name, vingtile, fill=la_name))
+e <- e + facet_wrap(~la_name, strip.position = 'bottom', scales = 'free_x',ncol = pperrow)
+e <- e + geom_violin(color="white") + ggtitle("England",subtitle="English Index of Multiple Deprivation 2015") +
   theme_classic() + theme(plot.subtitle = element_text(color = 'white', size = 18, face='italic'),plot.title = element_text(color = "white",size=26),strip.background = element_rect(fill = 'black'),strip.text = element_text(color='white',size=12),axis.text.y = element_blank(),axis.ticks.y = element_blank(), axis.title.y = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.title.x=element_blank(), legend.position="none",axis.title = element_text(color="white",size=12),plot.background=element_rect(fill="black"),panel.background = element_rect(fill="black"))
 
 # Save the plot to png file, A1 size
@@ -76,8 +80,8 @@ wlsoa_la_imd_vingtile <- wlsoa_la_imd %>%
 
 # Make the plot
 w <- ggplot(wlsoa_la_imd_vingtile, aes(la_name, vingtile, fill=la_name))
-w <- w + facet_wrap(~la_name, strip.position = 'bottom', scales = 'free_x',ncol = 6)
-w <- w + geom_violin(color="white") + ggtitle("Wales Local Authority Deprivation Profiles",subtitle="These charts show the distribution of Lower Super Output Areas by deprivation in Welsh local authorities. \nA fatter bottom indicates proportionally more more-deprived LSOAs. A fatter head indicate more less-deprived LSOAs. \n\n@northernjamie ") +
+w <- w + facet_wrap(~la_name, strip.position = 'bottom', scales = 'free_x',ncol = pperrow)
+w <- w + geom_violin(color="white") + ggtitle("Wales",subtitle="Welsh Index of Multiple Deprivation 2014") +
   theme_classic() + theme(plot.subtitle = element_text(color = 'white', size = 10, face='italic'),plot.title = element_text(color = "white",size=26),strip.background = element_rect(fill = 'black'),strip.text = element_text(color='white',size=8),axis.text.y = element_blank(),axis.ticks.y = element_blank(), axis.title.y = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.title.x=element_blank(), legend.position="none",axis.title = element_text(color="white",size=12),plot.background=element_rect(fill="black"),panel.background = element_rect(fill="black"))
 w
 
@@ -85,27 +89,43 @@ w
 
 ### Scotland
 
-scotland_imd <- read.csv("data/SIMD_DZ_LA.csv")
+scotland_imd <- read.csv("./data/SIMD_DZ_LA.csv")
 
 s_imd_vingtile <- scotland_imd %>%
   mutate(vingtile = ntile(rank, 20))
   
 s <- ggplot(s_imd_vingtile, aes(lalabel, vingtile, fill=lalabel))
-s <- s + facet_wrap(~lalabel, strip.position = 'bottom', scales = 'free_x',ncol = 7)
-s <- s + geom_violin(color="white") + ggtitle("Scotland LA Deprivation Profiles",subtitle="These charts show the distribution of Data Zones by deprivation in Scottish local authorities. \nA fatter bottom indicates proportionally more more-deprived DZs. A fatter head indicate more less-deprived DZs. \n\n@northernjamie ") +
+s <- s + facet_wrap(~lalabel, strip.position = 'bottom', scales = 'free_x',ncol = pperrow)
+s <- s + geom_violin(color="white") + ggtitle("Scotland",subtitle="Scottish Index of Multiple Deprivation 2016") +
   theme_classic() + theme(plot.subtitle = element_text(color = 'white', size = 10, face='italic'),plot.title = element_text(color = "white",size=26),strip.background = element_rect(fill = 'black'),strip.text = element_text(color='white',size=8),axis.text.y = element_blank(),axis.ticks.y = element_blank(), axis.title.y = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.title.x=element_blank(), legend.position="none",axis.title = element_text(color="white",size=12),plot.background=element_rect(fill="black"),panel.background = element_rect(fill="black"))
 s
 
 ### Northern Ireland
 
-niimd <- read.csv("data/NI_IMD_LGD_LSOA.csv")
+niimd <- read.csv("./data/NI_IMD_LGD_LSOA.csv")
 ni_imd_vingtile <- niimd %>%
   mutate(vingtile = ntile(rank,20))
 
 ni <- ggplot(ni_imd_vingtile, aes(LGD2014NAME, vingtile, fill=LGD2014NAME))
-ni <- ni + facet_wrap(~LGD2014NAME, strip.position = 'bottom', scales = 'free_x',ncol = 7)
-ni <- ni + geom_violin(color="white") + ggtitle("N. Ireland LA Deprivation Profiles",subtitle="These charts show the distribution of LSOAs by deprivation in Nothern Irish local authorities. \nA fatter bottom indicates proportionally more more-deprived LSOAs. A fatter head indicate more less-deprived LSOAs. \n\n@northernjamie ") +
+ni <- ni + facet_wrap(~LGD2014NAME, strip.position = 'bottom', scales = 'free_x',ncol = pperrow)
+ni <- ni + geom_violin(color="white") + ggtitle("Northern Ireland",subtitle="Northern Ireland Multiple Deprivation Measure 2017") +
   theme_classic() + theme(plot.subtitle = element_text(color = 'white', size = 10, face='italic'),plot.title = element_text(color = "white",size=26),strip.background = element_rect(fill = 'black'),strip.text = element_text(color='white',size=6),axis.text.y = element_blank(),axis.ticks.y = element_blank(), axis.title.y = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.title.x=element_blank(), legend.position="none",axis.title = element_text(color="white",size=12),plot.background=element_rect(fill="black"),panel.background = element_rect(fill="black"))
 ni
 
+# Make the big poster
+
+# 26 rows of charts 30 inches = 1.15 in per row
+## England 326 charts 21 rows
+## Wales 22 charts 2 rows
+## Scotland 32 charts 2 rows
+## NIreland 11 charts 1 row
+# 4 Country titles 4 Inches
+# 1 big title - 2 Inches
+# 36 Inches
+
+# Save the plot to png file, A1 size
+ggsave("engimd.png",e,width=24,height=24.15,units="in")
+ggsave("walesimd.png",w,width=24,height=3,units="in")
+ggsave("scotlandimd.png",s,width=24,height=4,units="in")
+ggsave("nirelandimd.png",ni,width=17.6,height=2,units="in")
 
