2ecb9923ea588c2ae9726ff53358d66edd8b9e92
northernjamie <northernjamie@gmail.com>
2019-09-25 08:52:21 +0100

    Big git push after neglecting it for some time

---
+++ 2019_IMD_prep_script.R
@@ -0,0 +1,95 @@
+# IMD lava lamps for 2019 with new data
+
+library(tidyverse)
+library(googlesheets)
+## Political control for councils
+
+### In this google sheet: https://docs.google.com/spreadsheets/d/1vkDwmBKL9P7m99oQXdieJzLFDna2XIhOFDWQ5gVoXq8/edit#gid=0
+
+pol_control <- gs_key("1vkDwmBKL9P7m99oQXdieJzLFDna2XIhOFDWQ5gVoXq8")
+pol_control <- gs_read(pol_control)
+pol_control <- pol_control %>%
+  select('ONS_LA_code','2019')
+
+## Get the lookups in
+
+lu_lsoa_la <- readr::read_csv("data/lookup_lsoa_la_2019.csv")
+lu_lsoa_ward <- readr::read_csv("data/lookup_lsoa_ward_2018.csv")
+lu_ward_pcon <- readr::read_csv("data/lookup_ward_westminster_constituency_2018.csv")
+lu_la_region <- readr::read_csv("data/lookup_la_region_2019.csv")
+
+## Get the IMD data
+
+### use the 2015 IMD data for now - 2019 is the same shape - just need to change the source file
+
+### IMD data at LSOA level
+
+imd_data_lsoa <- readr::read_csv("http://opendatacommunities.org/downloads/cube-table?uri=http%3A%2F%2Fopendatacommunities.org%2Fdata%2Fsocietal-wellbeing%2Fimd%2Findices")
+
+### IMD data at LA level rank of average rank
+
+imd_data_la <- readr::read_csv("http://opendatacommunities.org/downloads/cube-table?uri=http%3A%2F%2Fopendatacommunities.org%2Fdata%2Fsocietal-wellbeing%2Fimd%2Findicesbyla")
+
+## match the LSOA data to the LA data
+imd_data_lsoa_la <- imd_data_lsoa %>%
+  filter(Measurement == 'Rank') %>%
+  rename(domain = 'Indices of Deprivation') %>%
+  filter(domain == 'a. Index of Multiple Deprivation (IMD)') %>%
+  left_join(lu_lsoa_la, by = c('FeatureCode' = 'LSOA11CD')) %>%
+  left_join(imd_data_la, by =  c('LAD19CD' = 'FeatureCode')) %>%
+  rename(la_domain = 'Indices of Deprivation') %>%
+  filter(la_domain == 'a. Index of Multiple Deprivation (IMD)') %>%
+  filter(Measurement.y == 'Rank of average rank') %>%
+  rename('Rank_of_avg_rank' = 'Value.y') %>%
+  left_join(pol_control, by = c('LAD19CD' = 'ONS_LA_code')) %>%
+  rename(pol_control_2019 = '2019') %>%
+  left_join(lu_la_region, by = c('LAD19CD' = 'LAD19CD')) %>%
+  left_join(lu_lsoa_ward, by = c('FeatureCode' = 'LSOA11CD')) %>%
+  mutate(vigintile = ntile(Value.x, 20)) %>%
+  filter(LAD19CD != "E06000053") %>%
+  select('FeatureCode','LAD19CD','LAD19NM.x','vigintile','pol_control_2019','Rank_of_avg_rank')
+  
+
+#left_join(lu_ward_pcon, by = c('WD18CD' = 'WD18CD'))  
+
+
+
+
+### when doing this with the real data there should be 32,844 lsoas
+### There are duplicates eg lsoa E01000330. These seem to arise from the ward
+### to pcon lookup, where some wards straddle more than one consituency
+### Best thing to do is to maybe ignore pcon for the general data munging
+### and merge it in for that bit only.
+
+## set the colors for the parties
+palControl <- c(LAB = '#DC241f',LD = '#FAA61A',CON = '#0087DC',NOC = '#aaaaaa', UKIP = '#70147A', OTHER = 'pink', NPC = '#333333')
+
+
+## add the sort factor
+imd_data_lsoa_la$LAD19NM_IMD <- reorder(imd_data_lsoa_la$LAD19NM.x,imd_data_lsoa_la$Rank_of_avg_rank) 
+
+## Make the lava lamp plot
+eng_imd_pol_lava <- ggplot(imd_data_lsoa_la, aes(LAD19NM_IMD, vigintile, fill = pol_control_2019)) + 
+  facet_wrap(~ LAD19NM_IMD, strip.position = 'bottom', scales = 'free_x',ncol = 16) +
+  geom_violin(color = '#f7f1d4') +
+  scale_fill_manual(values = palControl) + 
+  theme(legend.position = 'none',
+        plot.subtitle = element_text(color = '#b1b1b1', size = 18, face='italic'),
+        plot.title = element_text(color = "#b1b1b1",size=36),
+        strip.background = element_rect(fill = '#f7f1d4'),
+        strip.text = element_text(color='#232323',size=8),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.title.x=element_blank(),
+        axis.title = element_text(color="#232323",size=12),
+        plot.background=element_rect(fill="#f7f1d4"),
+        panel.background = element_rect(fill="#f7f1d4"),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank())
+
+ggsave("engimdsortIMDfillpolitic3.svg",eng_imd_pol_lava,width=24,height=24.15,units="in")
+
+
--- Working_Script.R
+++ Working_Script.R
@@ -70,8 +70,8 @@ e <- ggplot(lsoa_pcon_imd_vingtile, aes(PCON15NM, vingtile, fill = PCON15NM))
 e <- e + facet_wrap(~ PCON15NM, strip.position = 'bottom', scales = 'free_x',ncol = pperrow)
 e <- e + geom_violin(color="white") + ggtitle("England",subtitle="English Index of Multiple Deprivation 2015.  Parliamentary Constituencies. \n@northernjamie \nData: MHCLG @ http://opendatacommunities.org") +
   theme_classic() + theme(plot.subtitle = element_text(color = 'white', size = 18, face='italic'),plot.title = element_text(color = "white",size=26),strip.background = element_rect(fill = 'black'),strip.text = element_text(color='white',size=8),axis.text.y = element_blank(),axis.ticks.y = element_blank(), axis.title.y = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.title.x=element_blank(), legend.position="none",axis.title = element_text(color="white",size=12),plot.background=element_rect(fill="black"),panel.background = element_rect(fill="black"))
-
-ggsave("engimdsortIMDpcon.png",e,width=24,height=24.15,units="in")
+e
+ggsave("engimdsortIMDpcon2.png",e,width=24,height=24.15,units="in")
 
 # Match the England LA dataframe to Latitude
 
@@ -104,7 +104,8 @@ e <- e + facet_wrap(~ la_name_order_IMD, strip.position = 'bottom', scales = 'fr
 e <- e + geom_violin(color = 'white') + ggtitle("England Local Authorities: Deprivation Profile vs Political Control",subtitle="English Index of Multiple Deprivation 2015. Local Authority areas sorted according to the deprivation of the area, ie top left is most deprived, bottom right least deprived. \nShapes show the deprivation profile of each area - wider bottom indicates more more-deprived LSOAs, wider top indicates more less-deprived LSOAs. \n@northernjamie \nData: MHCLG @ http://opendatacommunities.org & LGiU @ https://www.lgiu.org.uk/local-government-facts-and-figures/") +
   theme_classic() + theme(plot.subtitle = element_text(color = 'white', size = 18, face='italic'),plot.title = element_text(color = "white",size=36),strip.background = element_rect(fill = 'black'),strip.text = element_text(color='white',size=8),axis.text.y = element_blank(),axis.ticks.y = element_blank(), axis.title.y = element_blank(),axis.text.x = element_blank(),axis.ticks.x = element_blank(),axis.title.x=element_blank(), legend.position="bottom",legend.title=element_blank(),legend.background=element_rect(fill='#000000'),legend.key.size=unit(1,"cm"),legend.text = element_text(color='white',size=24),axis.title = element_text(color="white",size=12),plot.background=element_rect(fill="black"),panel.background = element_rect(fill="black"))
 e <- e + scale_fill_manual(values = palControl)
-ggsave("engimdsortIMDfillpolitic2.svg",e,width=24,height=24.15,units="in")
+e
+ggsave("engimdsortIMDfillpolitic3.svg",e,width=24,height=24.15,units="in")
 
 # Add the factor sorted by LA dep rank
 lsoa_la_imd_vingtile_noscilly$la_name_order_IMD <- reorder(lsoa_la_imd_vingtile_noscilly$la_name,lsoa_la_imd_vingtile_noscilly$rank_of_avg_rank) 
